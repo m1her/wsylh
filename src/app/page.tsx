ba0db600703e4e2b94464edc6915351a8c06b97c
m1her <maher.ple2@gmail.com>
2024-11-05 11:47:58 +0200

    remove framer motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
 "use client";
 import Link from "next/link";
-import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <div className="">
       <div>
         <Link href="https://exams.wsyelhi.com/register">asd</Link>
-        <motion.div
-          className="w-10 h-10 bg-blue-400"
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-        />
+       
       </div>
     </div>
   );
