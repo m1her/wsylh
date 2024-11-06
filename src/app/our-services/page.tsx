1c5c7ec0bd1060d6d5040c392929779a7bcfcffd
m1her <maher.ple2@gmail.com>
2024-11-06 20:36:32 +0200

    new design

--- src/app/our-services/page.tsx
+++ src/app/our-services/page.tsx
@@ -1,10 +1,11 @@
 import { Ad } from "@/components/Ad";
 import { FAQ } from "@/components/FAQ";
 import { HowItWorks } from "@/components/HowItWorks";
+import { OurServices } from "@/components/OurServices";
 import { Pricing } from "@/components/Pricing";
 import React from "react";
 
-const OurServices = () => {
+const Our_Services = () => {
   return (
     <div className="flex flex-col">
       <OurServices />
@@ -17,4 +18,4 @@ const OurServices = () => {
   );
 };
 
-export default OurServices;
+export default Our_Services;
