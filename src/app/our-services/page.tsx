f40fdeb75da4361dd34733032d4a81c66a901690
m1her <maher.ple2@gmail.com>
2024-11-07 21:01:11 +0200

    services

--- src/app/our-services/page.tsx
+++ src/app/our-services/page.tsx
@@ -1,9 +1,10 @@
+import { OurServices } from "@/components/OurServices";
 import React from "react";
 
 const Our_Services = () => {
   return (
     <div className="flex flex-col">
-
+      <OurServices />
     </div>
   );
 };
--- src/components/OurServices/index.tsx
+++ src/components/OurServices/index.tsx
@@ -8,13 +8,18 @@ export const OurServices = () => {
       id="SERVICE_SECTION"
       className="w-full bg-white min-h-screen py-12 md:px-14 px-10"
     >
-      <div className="flex flex-col items-center gap-6">
-        <div className="md:text-4xl text-3xl font-semibold">خدماتنا</div>
-        <div className="md:w-1/2 text-center md:text-xl text-lg text-gray-400">
-          اعداد اختبارات ورقية مع نماذج الاجابة ما عليك سوى الدخول على الموقع
-          واختيار المرحلة وستحصل على المزايا التالية
+      <div className="flex flex-col items-center gap-6 ">
+        <div className="flex flex-col items-center gap-6 ">
+          <div className="md:text-4xl text-3xl font-semibold">
+            ما نقدمه في إعداد الاختبارات
+          </div>
+          <div className="w-32 md:h-1 h-0.5 rounded-full bg-secondary"></div>
+          <div className="md:w-1/2 text-center md:text-xl text-lg">
+            نظام متكامل لإنشاء الاختبارات الورقية مع نماذج الاجابة ما عليك سوى
+            الدخول على الموقع والبدء باستخدام موقعنا و ستحصل على الخدمات التالية
+          </div>
         </div>
-        <div className="w-32 md:h-1 h-0.5 rounded-full bg-secondary"></div>
+
         <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 md:gap-6 gap-2">
           {servicesData.map((item, idx) => (
             <ServiceCard
